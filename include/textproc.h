f7601b216e9c40391f13f689847752712e6e0c0d
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-04 20:03:48 +0800

    docs: document other function in textproc.h

--- include/textproc.h
+++ include/textproc.h
@@ -10,10 +10,26 @@
 #define TP_LOWER 0
 #define TP_UPPER 1
 
-
+/*
+ * Reverse str and write the reversed string to out.
+ */
 void textproc_reverse_string(char *str, char *out, size_t bufsiz);
+
+/*
+ * Perform rot13 encoding on str, and write it to out.
+ */
 void textproc_rot13_encode(char *str, char *out, size_t bufsiz);
+
+/*
+ * Decode a rot13-encoded string in str, and write it to out.
+ */
 void textproc_rot13_decode(char *str, char *out, size_t bufsiz);
+
+/*
+ * Shift letters in a string by a certain number of characters. The
+ * textproc_rot13_encode and decode functions call this function with
+ * an offset of 13.
+ */
 void textproc_shift_text(char *str, char *out, size_t bufsiz, int offset);
 
 /*
