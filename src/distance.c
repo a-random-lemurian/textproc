2a486fb8d5451f419d8c9c41856d329fdea7cd3b
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-05 23:09:06 +0800

    fix: fix mixed code and declarations

--- src/distance.c
+++ src/distance.c
@@ -7,17 +7,16 @@ size_t textproc_hamming_distance(char *s1, char *s2)
   size_t dist = 0;
   size_t s1_siz = strlen(s1);
   size_t s2_siz = strlen(s2);
+  size_t pos;
+
 
   if (s1_siz != s2_siz) {
     fprintf(stderr,
             "textproc_hamming_distance: s1 and s2 lengths not same "
-            "(s1 len %ld, s2 len %ld)",
+            "(s1 len %ld, s2 len %ld)\n",
             s1_siz, s2_siz);
     abort();
   }
-
-
-  size_t pos;
 
   for (pos = 0; pos < (s1_siz + s2_siz) / 2; pos++) {
     if (s1[pos] != s2[pos]) {
