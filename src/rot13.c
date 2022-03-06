bf3cfcd0b23b90eada1a978d99861be474c7d94d
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-07 00:00:34 +0800

    docs: add note about rot13 functions

--- src/rot13.c
+++ src/rot13.c
@@ -1,5 +1,10 @@
 #include <textproc.h>
 
+/*
+ * The textproc_rot13_encode and decode functions are essentially wrappers
+ * for textproc_shift_text, with an offset of 13.
+ */
+
 void textproc_rot13_encode(char *str, char *out, size_t bufsiz)
 {
   textproc_shift_text(str, out, bufsiz, 13);
